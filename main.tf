fd6fd0f04a849ce7a9bcc71e94c06dbf86377901
Felippe <mendonca.felippe@gmail.com>
2019-05-15 10:13:34 -0300

    simplified the way of importing existing networking resources

--- main.tf
+++ main.tf
@@ -8,21 +8,21 @@ provider "openstack" {
   version     = "~> 1.4"
 }
 
-resource "openstack_compute_keypair_v2" "keypair" {
-  name       = "jfed_key"
-  public_key = "${file("etc/id_rsa_jfed.pub")}"
-}
-
 data "openstack_identity_user_v3" "admin" {
   name = "admin"
 }
 
-data "terraform_remote_state" "networking" {
-  backend = "local"
+data "openstack_networking_network_v2" "provider" {
+  name = "provider"
+}
 
-  config = {
-    path = "${path.module}/networking-resources/terraform.tfstate"
-  }
+data "openstack_networking_subnet_v2" "provider" {
+  name = "provider"
+}
+
+resource "openstack_compute_keypair_v2" "keypair" {
+  name       = "jfed_key"
+  public_key = "${file("etc/id_rsa_jfed.pub")}"
 }
 
 # --- Instances
@@ -30,13 +30,13 @@ resource "openstack_networking_port_v2" "instance_ports" {
   count = "${length(var.instances)}"
 
   name           = "${lookup(var.instances[count.index], "instance_name")}"
-  network_id     = "${data.terraform_remote_state.networking.provider_net_id}"
+  network_id     = "${data.openstack_networking_network_v2.provider.id}"
   mac_address    = "${lookup(var.instances[count.index], "mac_address")}"
   admin_state_up = "true"
 
   fixed_ip = [
     {
-      subnet_id  = "${data.terraform_remote_state.networking.provider_subnet_id}"
+      subnet_id  = "${data.openstack_networking_subnet_v2.provider.id}"
       ip_address = "${lookup(var.instances[count.index], "ip_address")}"
     },
   ]
@@ -56,10 +56,8 @@ resource "openstack_compute_instance_v2" "instances" {
   }
 }
 
-
 resource "null_resource" "start_command" {
   count = "${length(var.instances)}"
-
 
   connection {
     host        = "${element(openstack_compute_instance_v2.instances.*.access_ip_v4, count.index)}"
@@ -68,9 +66,7 @@ resource "null_resource" "start_command" {
     private_key = "${file("etc/id_rsa_jfed")}"
   }
 
-
   provisioner "remote-exec" {
     inline = "sudo /futebol/vms/${lookup(var.instances[count.index], "instance_name")}/run.bash"
   }
 }
-
--- networking-resources/import_resources.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-terraform import openstack_networking_subnet_v2.provider ace752cb-978a-4fc8-a92e-f44c979d691a
-terraform import openstack_networking_network_v2.provider 780a1605-d26b-48e9-b17b-a5f4eb316623
